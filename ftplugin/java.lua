33c36b832ce3cc6374a84da1dab75185e639da89
Jose Vega <josevega234@gmail.com>
2025-03-28 05:03:56 +0100

    fixes java

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,27 +2,27 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.cmdheight = 4 -- more space in the neovim command line for displaying messages
 
-local status = pcall(require, "jdtls")
-if not status then
-  return
-end
-
-local home = os.getenv("HOME")
-
-local dataFolder = home .. "/.local/share/nvim"
-
-local config = {
-  -- cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
-  cmd = {
-    "bash",
-    home .. "/.config/nvim/scripts/java-lsp.bash",
-    dataFolder,
-    "java",
-  },
-  root_dir = vim.fs.dirname(vim.fs.find({ ".gradlew", ".git", "mvnw" }, { upward = true })[1]),
-}
-require("jdtls").start_or_attach(config)
-
+-- local status = pcall(require, "jdtls")
+-- if not status then
+--   return
+-- end
+--
+-- local home = os.getenv("HOME")
+--
+-- local dataFolder = home .. "/.local/share/nvim"
+--
+-- local config = {
+--   -- cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
+--   cmd = {
+--     "bash",
+--     home .. "/.config/nvim/scripts/java-lsp.bash",
+--     dataFolder,
+--     "java",
+--   },
+--   root_dir = vim.fs.dirname(vim.fs.find({ ".gradlew", ".git", "mvnw" }, { upward = true })[1]),
+-- }
+-- require("jdtls").start_or_attach(config)
+--
 -- local status = pcall(require, "jdtls")
 -- if not status then
 --   return
