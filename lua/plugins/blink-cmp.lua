6c13f296ada583dfcf2bf54116da2ac077c659d9
Jose Vega <josevega234@gmail.com>
2025-09-11 23:20:14 +0200

    copilot

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,43 +1,36 @@
-return { -- Autocompletion
+return {
   'saghen/blink.cmp',
-  event = 'VimEnter',
-  version = '1.*',
+  -- optional: provides snippets for the snippet source
   dependencies = {
     'folke/lazydev.nvim',
     'MahanRahmati/blink-nerdfont.nvim',
     'moyiz/blink-emoji.nvim',
-    'Kaiser-Yang/blink-cmp-avante',
+    'rafamadriz/friendly-snippets',
   },
-  --- @module 'blink.cmp'
-  --- @type blink.cmp.Config
-  opts = {
-    keymap = {
-      -- 'default' (recommended) for mappings similar to built-in completions
-      --   <c-y> to accept ([y]es) the completion.
-      --    This will auto-import if your LSP supports it.
-      --    This will expand snippets if the LSP sent a snippet.
-      -- 'super-tab' for tab to accept
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- For an understanding of why the 'default' preset is recommended,
-      -- you will need to read `:help ins-completion`
-      --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
-      --
-      -- All presets have the following mappings:
-      -- <tab>/<s-tab>: move to right/left of your snippet expansion
-      -- <c-space>: Open menu or open docs if already open
-      -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-      -- <c-e>: Hide menu
-      -- <c-k>: Toggle signature help
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
-      preset = 'enter',
 
-      -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-      --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-    },
+  -- use a release tag to download pre-built binaries
+  version = '1.*',
+  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'default' },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -45,15 +38,13 @@ return { -- Autocompletion
       nerd_font_variant = 'mono',
     },
 
-    completion = {
-      -- By default, you may press `<c-space>` to show the documentation.
-      -- Optionally, set `auto_show = true` to show the documentation after a delay.
-      documentation = { auto_show = false, auto_show_delay_ms = 500 },
-    },
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = { documentation = { auto_show = false } },
 
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
       default = {
-        'avante',
         'lsp',
         'nerdfont',
         'emoji',
@@ -62,13 +53,13 @@ return { -- Autocompletion
         'lazydev',
       },
       providers = {
-        avante = {
-          module = 'blink-cmp-avante',
-          name = 'Avante',
-          opts = {
-            -- options for blink-cmp-avante
-          },
-        },
+        -- avante = {
+        --   module = 'blink-cmp-avante',
+        --   name = 'Avante',
+        --   opts = {
+        --     -- options for blink-cmp-avante
+        --   },
+        -- },
         lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
         nerdfont = {
           module = 'blink-nerdfont',
@@ -99,20 +90,14 @@ return { -- Autocompletion
       },
     },
 
-    -- snippets = { preset = 'luasnip' },
-
-    -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-    -- which automatically downloads a prebuilt binary when enabled.
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
     --
-    -- By default, we use the Lua implementation instead, but you may enable
-    -- the rust implementation via `'prefer_rust_with_warning'`
-    --
-    -- See :h blink-cmp-config-fuzzy for more information
-    fuzzy = { implementation = 'prefer_rust' },
-
-    -- Shows a signature help window while you type arguments for a function
-    signature = { enabled = true },
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
   },
+  opts_extend = { 'sources.default' },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,54 @@
 return {
-  'zbirenbaum/copilot.lua',
-  cmd = 'Copilot',
-  event = 'BufEnter',
-  opts = { suggestion = { auto_trigger = true, debounce = 150 } },
+  {
+    'zbirenbaum/copilot.lua',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('copilot').setup({
+        suggestion = {
+          auto_trigger = true,
+          keymap = {
+            accept = '<C-y>',
+          },
+        },
+      })
+    end,
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' },
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    config = function()
+      require('CopilotChat').setup({
+        model = 'claude-sonnet-4',
+        highlight_headers = false,
+        prompts = {
+          GoodPractices = {
+            prompt = 'Apply good practices',
+            system_prompt = 'Refactor the following code to improve its clarity and readability, while maintaining its functionality. Remove any switch case in favor of object literal access, use nested ternaries, use implicit arrow return if needed. Be sure to include comments explaining the changes you made.',
+            description = 'Refactor the code to apply good practices',
+          },
+        },
+      })
+    end,
+    opts = {},
+    cmd = {
+      'CopilotChat',
+      'CopilotChatOpen',
+      'CopilotChatToggle',
+      'CopilotChatLoad',
+      'CopilotChatPrompts',
+      'CopilotChatModels',
+      'CopilotChatAgents',
+      'CopilotChatReview',
+      'CopilotChatFix',
+      'CopilotChatOptimize',
+      'CopilotChatDocs',
+      'CopilotChatTests',
+      'CopilotChatCommit',
+    },
+  },
 }
