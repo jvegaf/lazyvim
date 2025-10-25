2251a4b8ef4e36a3d1c27130046453206ba34ca7
Jose Vega <josevega234@gmail.com>
2025-10-26 00:24:37 +0200

    fck angular

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -42,7 +42,6 @@ require('lazy').setup({
     { import = 'lazyvim.plugins.extras.lang.docker' },
     { import = 'lazyvim.plugins.extras.lang.markdown' },
     { import = 'lazyvim.plugins.extras.lang.typescript' },
-    { import = 'lazyvim.plugins.extras.lang.angular' },
     -- { import = 'lazyvim.plugins.extras.lang.python' },
     { import = 'lazyvim.plugins.extras.lang.git' },
     { import = 'lazyvim.plugins.extras.lang.clangd' },
