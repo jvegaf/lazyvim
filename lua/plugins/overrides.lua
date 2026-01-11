ac341b2edacbef20331dabd08f4c841c9eecccf1
Jose Vega <josevega234@gmail.com>
2026-01-11 15:48:55 +0100

    symbols outline

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -4,6 +4,16 @@ return {
     'folke/trouble.nvim',
     opts = { use_diagnostic_signs = true }, -- Use diagnostic signs for trouble.nvim
   },
+  -- Add symbols-outline.nvim plugin
+  {
+    -- Plugin: symbols-outline.nvim
+    -- URL: https://github.com/simrat39/symbols-outline.nvim
+    -- Description: A tree like view for symbols in Neovim using the Language Server Protocol.
+    'simrat39/symbols-outline.nvim',
+    cmd = 'SymbolsOutline', -- Command to open the symbols outline
+    keys = { { '<leader>cs', '<cmd>SymbolsOutline<cr>', desc = 'Symbols Outline' } }, -- Keybinding to open the symbols outline
+    config = true, -- Use default configuration
+  },
   -- {
   --   'CopilotC-Nvim/CopilotChat.nvim',
   --   opts = {
