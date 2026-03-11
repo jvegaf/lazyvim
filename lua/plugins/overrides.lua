d23d7818332ec3c35175d688b6d4864cd8387a69
Jose Vega <josevega234@gmail.com>
2026-03-11 19:03:06 +0100

    nvim tree back

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -1,5 +1,5 @@
 return {
-  -- { 'nvim-neo-tree/neo-tree.nvim', enabled = false },
+  { 'nvim-neo-tree/neo-tree.nvim', enabled = false },
   {
     'folke/trouble.nvim',
     opts = { use_diagnostic_signs = true }, -- Use diagnostic signs for trouble.nvim
