27e7872c64ae505a424d0d84bf6b53bd3ef278ef
Jose Vega <josevega234@gmail.com>
2026-05-21 23:37:43 +0200

    dissable refactoring plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,7 +34,7 @@ require('lazy').setup({
     { import = 'lazyvim.plugins.extras.editor.inc-rename' },
     { import = 'lazyvim.plugins.extras.editor.mini-diff' },
     { import = 'lazyvim.plugins.extras.editor.outline' },
-    { import = 'lazyvim.plugins.extras.editor.refactoring' },
+    -- { import = 'lazyvim.plugins.extras.editor.refactoring' },
     { import = 'lazyvim.plugins.extras.editor.snacks_picker' },
     { import = 'lazyvim.plugins.extras.formatting.biome' },
     { import = 'lazyvim.plugins.extras.lang.clangd' },
