e66503ee553d35f2e30c56bda882d59122f1f0ed
Jose Vega <josevega234@gmail.com>
2026-06-14 03:16:48 +0200

    updates HAABVsssssssssssssssssssssssssssssaaooooooooooooooooooooooooooooooooooooooooooooa

---
+++ lua/plugins/jc.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'artur-shaik/jc.nvim',
+    ft = { 'java' },
+    dependencies = {
+      'nvim-java/nvim-java',
+    },
+    opts = {
+      keys_prefix = "'j",
+    },
+  },
+}
