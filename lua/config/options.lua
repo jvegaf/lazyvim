e39b38f0a09322b72d599235c7166dd5fee3753c
Jose Vega <josevega234@gmail.com>
2026-09-02 12:18:47 +0200

    rollback blink -> cmp classic

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -93,7 +93,7 @@ local options = {
 --   vim.opt[k] = v
 -- end
 
-vim.g.lazyvim_blink_main = true
+vim.g.lazyvim_blink_main = false
 -- In case you don't want to use `:LazyExtras`,
 -- then you need to set the option below.
 vim.g.lazyvim_picker = 'snacks'
--- lua/plugins/disabled/blink-cmp.lua
+++ lua/plugins/disabled/blink-cmp.lua
@@ -1,25 +1,16 @@
-local icons = require('utils.icons')
 return {
   {
     'saghen/blink.cmp',
-    lazy = false,
-    version = '1.*',
-    dependencies = { 'rafamadriz/friendly-snippets' },
+    dependencies = {
+      'MahanRahmati/blink-nerdfont.nvim',
+      {
+        'mikavilpas/blink-ripgrep.nvim',
+        version = '*', -- use the latest stable version
+      },
+      'alexandre-abrioux/blink-cmp-npm.nvim',
+    },
     opts = {
-      appearance = {
-        kind_icons = icons.trouble.kinds,
-      },
-      keymap = {
-        preset = 'none',
-        ['<CR>'] = { 'accept', 'fallback' },
-        ['<Tab>'] = { 'select_next', 'fallback' },
-        ['<S-Tab>'] = { 'select_prev', 'fallback' },
-        ['<Up>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<Down>'] = { 'scroll_documentation_down', 'fallback' },
-        ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
-        -- stylua: ignore
-        ['<C-space>'] = { function(cmp) cmp.show({ providers = { 'snippets' } }) end },
-      },
+      fuzzy = { implementation = 'rust' },
       cmdline = {
         completion = {
           list = { selection = { preselect = false } },
@@ -30,153 +21,182 @@ return {
         menu = {
           border = 'rounded',
           direction_priority = { 'n', 's' },
-          draw = { treesitter = { 'lsp' } },
-        },
-        documentation = {
-          auto_show = true,
-          window = { border = 'rounded' },
-        },
-        trigger = {
-          show_on_keyword = true,
-          show_on_accept_on_trigger_character = true,
-          show_on_insert_on_trigger_character = true,
-          show_on_trigger_character = true,
-          show_on_blocked_trigger_characters = { ' ', '\n', '\t' },
-        },
-        ghost_text = { enabled = false },
-        list = { selection = { preselect = false } },
-      },
-      fuzzy = {
-        implementation = 'prefer_rust',
-        sorts = {
-          'exact',
-          'score',
-          'sort_text',
-        },
-      },
-      signature = {
-        enabled = true,
-        trigger = { show_on_insert = true },
-        window = {
-          border = 'rounded',
-          direction_priority = { 's', 'n' },
-          show_documentation = true,
+          draw = {
+            columns = {
+              { 'label', 'label_description', gap = 1 },
+              { 'kind_icon', 'kind' },
+            },
+          },
         },
       },
       sources = {
-        default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+        default = {
+          'lazydev',
+          'lsp',
+          'path',
+          'snippets',
+          'buffer',
+          'nerdfont',
+          'ripgrep',
+          'npm',
+        },
         providers = {
-          buffer = {
-            name = 'Buffer',
-            enabled = true,
-            max_items = 3,
-            module = 'blink.cmp.sources.buffer',
-            min_keyword_length = 2,
-            score_offset = 65, -- the higher the number, the higher the priority
-            transform_items = function(a, items)
-              local keyword = a.get_keyword()
-              local correct, case
-              if keyword:match('^%l') then
-                correct = '^%u%l+$'
-                case = string.lower
-              elseif keyword:match('^%u') then
-                correct = '^%l+$'
-                case = string.upper
-              else
-                return items
-              end
+          lsp = {
+            name = 'LSP',
+            module = 'blink.cmp.sources.lsp',
+            -- You may enable the buffer source, when LSP is available, by setting this to `{}`
+            -- You may want to set the score_offset of the buffer source to a lower value, such as -5 in this case
+            fallbacks = { 'buffer' },
+            opts = { tailwind_color_icon = '██' },
 
-              -- avoid duplicates from the corrections
-              local seen = {}
-              local out = {}
-              for _, item in ipairs(items) do
-                local raw = item.insertText
-                if raw and raw:match(correct) then
-                  local text = case(raw:sub(1, 1)) .. raw:sub(2)
-                  item.insertText = text
-                  item.label = text
-                end
-                if not seen[item.insertText] then
-                  seen[item.insertText] = true
-                  table.insert(out, item)
-                end
-              end
-              return out
-            end,
+            --- These properties apply to !!ALL sources!!
+            --- NOTE: All of these options may be functions to get dynamic behavior
+            --- See the type definitions for more information
+            enabled = true, -- Whether or not to enable the provider
+            async = false, -- Whether we should show the completions before this provider returns, without waiting for it
+            timeout_ms = 2000, -- How long to wait for the provider to return before showing completions and treating it as asynchronous
+            transform_items = nil, -- Function to transform the items before they're returned
+            should_show_items = true, -- Whether or not to show the items
+            max_items = nil, -- Maximum number of items to display in the menu
+            -- Minimum number of characters in the keyword to trigger the provider
+            -- May also be a function(ctx: blink.cmp.Context): number
+            -- To ignore this property when manually showing the menu, set it like:
+            -- min_keyword_length = function(ctx) return ctx.trigger.initial_kind == 'manual' and 0 or 1 end
+            min_keyword_length = 0,
+            -- If this provider returns 0 items, it will fallback to these providers.
+            -- If multiple providers fallback to the same provider, all of the providers must return 0 items for it to fallback
+            fallbacks = {},
+            score_offset = 0, -- Boost/penalize the score of the items
+            override = nil, -- Override the source's functions
           },
+
           path = {
-            name = 'Path',
             module = 'blink.cmp.sources.path',
-            score_offset = 70,
-            min_keyword_length = 2,
-            fallbacks = { 'snippets', 'buffer' },
+            score_offset = 3,
+            fallbacks = { 'buffer' },
             opts = {
-              trailing_slash = false,
+              trailing_slash = true,
               label_trailing_slash = true,
               get_cwd = function(context)
                 return vim.fn.expand(('#%d:p:h'):format(context.bufnr))
               end,
-              show_hidden_files_by_default = true,
+              show_hidden_files_by_default = false,
+              -- Treat `/path` as starting from the current working directory (cwd) instead of the root of your filesystem
+              ignore_root_slash = false,
+              -- Maximum number of files/directories to return. This limits memory use and responsiveness for very large folders.
+              max_entries = 10000,
             },
           },
+
           snippets = {
-            name = 'Snippets',
             module = 'blink.cmp.sources.snippets',
-            min_keyword_length = 2,
-            score_offset = 85,
+            score_offset = -1, -- receives a -3 from top level snippets.score_offset
+
+            -- For `snippets.preset == 'default'`
+            opts = {
+              friendly_snippets = true,
+              search_paths = { vim.fn.stdpath('config') .. '/snippets' },
+              global_snippets = { 'all' },
+              extended_filetypes = {},
+              filter_snippets = function(filetype, file)
+                return true
+              end,
+              get_filetype = function(context)
+                return vim.bo.filetype
+              end,
+              -- Set to '+' to use the system clipboard, or '"' to use the unnamed register
+              clipboard_register = nil,
+              -- Whether to put the snippet description in the label description
+              use_label_description = false,
+            },
           },
-          dadbod = {
-            name = 'Dadbod',
-            module = 'vim_dadbod_completion.blink',
-            min_keyword_length = 2,
-            score_offset = 80,
+
+          buffer = {
+            module = 'blink.cmp.sources.buffer',
+            score_offset = -3,
+            opts = {
+              -- default to all visible buffers
+              get_bufnrs = function()
+                return vim
+                  .iter(vim.api.nvim_list_wins())
+                  :map(function(win)
+                    return vim.api.nvim_win_get_buf(win)
+                  end)
+                  :filter(function(buf)
+                    return vim.bo[buf].buftype ~= 'nofile'
+                  end)
+                  :totable()
+              end,
+              -- buffers when searching with `/` or `?`
+              get_search_bufnrs = function()
+                return { vim.api.nvim_get_current_buf() }
+              end,
+              -- Maximum total number of characters (in an individual buffer) for which buffer completion runs synchronously. Above this, asynchronous processing is used.
+              max_sync_buffer_size = 20000,
+              -- Maximum total number of characters (in an individual buffer) for which buffer completion runs asynchronously. Above this, the buffer will be skipped.
+              max_async_buffer_size = 200000,
+              -- Maximum text size across all buffers (default: 500KB)
+              max_total_buffer_size = 500000,
+              -- Order in which buffers are retained for completion, up to the max total size limit (see above)
+              retention_order = { 'focused', 'visible', 'recency', 'largest' },
+              -- Cache words for each buffer which increases memory usage but drastically reduces cpu usage. Memory usage depends on the size of the buffers from `get_bufnrs`. For 100k items, it will use ~20MBs of memory. Invalidated and refreshed whenever the buffer content is modified.
+              use_cache = true,
+              -- Whether to enable buffer source in substitute (:s), global (:g) and grep commands (:grep, :vimgrep, etc.).
+              -- Note: Enabling this option will temporarily disable Neovim's 'inccommand' feature
+              -- while editing Ex commands, due to a known redraw issue (see neovim/neovim#9783).
+              -- This means you will lose live substitution previews when using :s, :smagic, or :snomagic
+              -- while buffer completions are active.
+              enable_in_ex_commands = false,
+            },
           },
-          lsp = {
-            name = 'lsp',
-            enabled = true,
-            module = 'blink.cmp.sources.lsp',
-            score_offset = 95,
+
+          cmdline = {
+            module = 'blink.cmp.sources.cmdline',
           },
-          lazydev = {
-            name = 'LazyDev',
-            module = 'lazydev.integrations.blink',
+
+          omni = {
+            module = 'blink.cmp.sources.complete_func',
+            enabled = function()
+              return vim.bo.omnifunc ~= 'v:lua.vim.lsp.omnifunc'
+            end,
+            opts = {
+              complete_func = function()
+                return vim.bo.omnifunc
+              end,
+            },
+          },
+
+          nerdfont = {
+            module = 'blink-nerdfont',
+            name = 'Nerd Fonts',
+            score_offset = 15, -- Tune by preference
+            opts = {
+              insert = true, -- Insert nerdfont icon (default) or complete its name
+              trigger = ':-)', -- Customize the trigger. Defaults to ":"
+            },
+          },
+          ripgrep = {
+            module = 'blink-ripgrep',
+            name = 'Ripgrep',
+            opts = {},
+          },
+          npm = {
+            name = 'npm',
+            module = 'blink-cmp-npm',
+            async = true,
+            -- optional - make blink-cmp-npm completions top priority (see `:h blink.cmp`)
             score_offset = 100,
+            -- optional - blink-cmp-npm config
+            opts = {
+              ignore = {},
+              only_semantic_versions = true,
+              only_latest_version = false,
+            },
           },
         },
       },
     },
-    opts_extend = { 'sources.default' },
   },
-  -- {
-  --   'L3MON4D3/LuaSnip',
-  --   dependencies = { 'rafamadriz/friendly-snippets' },
-  --   event = 'InsertEnter',
-  --   postinstall = 'make install_jsregexp',
-  --   config = function()
-  --     local luasnip = require('luasnip')
-  --     luasnip.setup({
-  --       history = true,
-  --       updateevents = 'TextChanged,TextChangedI',
-  --       enable_autosnippets = true,
-  --     })
-  --     -- add vscode exported completions
-  --     require('luasnip.loaders.from_vscode').lazy_load()
-  --     require('luasnip.loaders.from_vscode').lazy_load('./snippets')
-  --     local r = require('utils.remaps')
-  --
-  --     r.map({ 'i', 's' }, '<c-n>', function()
-  --       if luasnip.expand_or_jumpable() then
-  --         luasnip.expand_or_jump()
-  --       end
-  --     end, 'Expand current snippet or jump to next', { silent = true })
-  --
-  --     r.map({ 'i', 's' }, '<c-p>', function()
-  --       if luasnip.jumpable(-1) then
-  --         luasnip.jump(-1)
-  --       end
-  --     end, 'Go to previous snippet', { silent = true })
-  --   end,
-  -- },
   {
     'folke/lazydev.nvim',
     ft = 'lua',
